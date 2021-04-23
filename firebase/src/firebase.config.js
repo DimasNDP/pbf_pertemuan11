e191612cf57c7cc8dde7d490685387ae8854f4ed
DimasNDP <nugraha.dimas312@gmail.com>
2021-04-23 20:25:43 +0700

    initial commit

---
+++ firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyA03EYTcP_a1ePGeN37Q4HPm7Wxw2bB-hI",
+    authDomain: "pbf-pertemuan11-2cc24.firebaseapp.com",
+    projectId: "pbf-pertemuan11-2cc24",
+    storageBucket: "pbf-pertemuan11-2cc24.appspot.com",
+    messagingSenderId: "1015965311944",
+    appId: "1:1015965311944:web:f9584b3187d29d4f6556fd",
+    measurementId: "G-0VB3JVKSYK"
+}
